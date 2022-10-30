d7e1d169d11208b4313477dc00aa81688bd41a41
Wis13 <vkqa@outlook.com>
2022-10-30 13:18:47 -0700

    Add first test

--- Pages/MainPage.py
+++ Pages/MainPage.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from Pages.BasePage import BasePage
 
 
@@ -6,3 +8,9 @@ class MainPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
+    NAME = "PRODUCTS"
+
+
+class MainPageLocators:
+    NAME_MAIN_PAGE = (By.XPATH, "//span[@class='title']")
+    LIST_OF_ITEMS = (By.XPATH, "//div[@class='inventory_item_description']")
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,12 @@
+from Pages import BasePage
+from Pages.MainPage import MainPage, MainPageLocators
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
-from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestMainPage:
+
+    def test_name(self):
+        driver = MainPage(self.driver)
+        list_items = driver.get_elements(MainPageLocators.LIST_OF_ITEMS)
+        assert driver.get_element_text(MainPageLocators.NAME_MAIN_PAGE) == MainPage.NAME
+        assert len(list_items) == 6
